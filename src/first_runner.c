4d3019b17fbabb2ea553846ef9f22b5bb1148496
robot <robot@robot>
2016-11-08 15:04:38 +0000

    Resolve first runner problems and utils

--- src/first_runner.c
+++ src/first_runner.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include "config.h"
+
 int is_in_arena(state *s, int *pos) {
 
 	return ((pos[0] > CENTER_POSITION) && (pos[0] < MAX_WIDTH_POSITION) && (pos[1] > CENTER_POSITION) && (pos[1] > MAX_HEIGHT_POSITION));
