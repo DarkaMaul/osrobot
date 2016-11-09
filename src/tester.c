2e1b29e5f0559e0004682c3ea2d0437cf3677a80
astrubel <axel.strubel@telecom-bretagne.eu>
2016-11-09 19:14:50 +0100

    Added speed of the motor and sleep to make the simple test work

--- src/tester.c
+++ src/tester.c
@@ -6,25 +6,24 @@
 
 int testRobot()
 {
-    ev3_motor_ptr a, b, c;
-    ev3_motor_ptr motors = ev3_load_motors();
-    ev3_motor_ptr motor = motors;
+	//	ev3_motor_ptr a, b, c;
+	ev3_motor_ptr motors = ev3_load_motors();
+	ev3_motor_ptr motor = motors;
 
-    FILE* fp = fopen("logs/motortest.txt", "w");
+	FILE* fp = fopen("logs/motortest.txt", "w");
 
-    while (motor)
-    {
-        ev3_reset_motor(motor);
-        ev3_open_motor(motor);
+	while (motor)
+	{
+		ev3_reset_motor(motor);
 
-        ev3_command_motor_by_name(motor, "run-forever");
-
-        motor = motor->next;
-
-
-        fprintf(fp, "Motor openned\n");
-    }
-/*
+		ev3_open_motor(motor);
+		ev3_set_speed_sp( motor, -500 );
+		ev3_command_motor_by_name(motor, "run-forever");
+		motor = motor->next;
+		fprintf(fp, "Motor opened\n");
+	}
+	sleep(3);
+	/*
     a = ev3_search_motor_by_port(motors, 'A');
     b = ev3_search_motor_by_port(motors, 'B');
     c = ev3_search_motor_by_port(motors, 'C');
@@ -38,10 +37,10 @@ int testRobot()
     ev3_stop_command_motor_by_name(a, "hold");
     ev3_stop_command_motor_by_name(b, "hold");
     ev3_stop_command_motor_by_name(c, "hold");
-*/
-    ev3_delete_motors(motors);
+	 */
+	ev3_delete_motors(motors);
 
-    fclose(fp);
+	fclose(fp);
 
-    return 0;
+	return 0;
 }
