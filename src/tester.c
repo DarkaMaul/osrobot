6a89a3cff0d90fab4242c3e6a6a6c5e6a1e394a9
astrubel <axel.strubel@telecom-bretagne.eu>
2016-11-14 15:15:46 +0100

    added a test found on ev3c github to test the sensors

--- src/tester.c
+++ src/tester.c
@@ -4,79 +4,8 @@
 #include "ev3c.h"
 #include "tester.h"
 
-/**
- * Theoretical max speed 1560 counts/s for the small motor
- */
 /*
-int grab(int absoluteposition, int speed)
-{
-	ev3_motor_ptr a;
-	ev3_motor_ptr motors = ev3_load_motors();
-
-	FILE* fp = fopen("logs/motortest.txt", "w");
-
-	a = ev3_search_motor_by_port(motors, 'A');
-	ev3_open_motor(a);
-	for(int i=0;i<2;i++){
-		if(i==1) {
-			speed=-speed;
-			absoluteposition=-absoluteposition;
-		}
-		ev3_reset_motor(a);
-		ev3_set_speed_sp( a, -1*speed );
-		ev3_set_position_sp(a, absoluteposition);
-		ev3_command_motor_by_name(a, "run-to-abs-pos");
-
-		while (ev3_motor_state(a) & MOTOR_RUNNING);
-	}
-	fprintf(fp, "Motor opened\n");
-
-
-
-	ev3_delete_motors(motors);
-
-	fclose(fp);
-
-	return 0;
-}
-
-
-*
- * Theoretical max speed 1560 counts/s for the small motor
-
-int release(int absoluteposition, int speed)
-{
-	ev3_motor_ptr a;
-	ev3_motor_ptr motors = ev3_load_motors();
-
-	FILE* fp = fopen("logs/motortest.txt", "w");
-
-	a = ev3_search_motor_by_port(motors, 'A');
-	ev3_open_motor(a);
-	for(int i=0;i<2;i++){
-		if(i==1) {
-			speed=-speed;
-			absoluteposition=-absoluteposition;
-		}
-		ev3_reset_motor(a);
-		ev3_set_speed_sp( a, -1*speed );
-		ev3_set_position_sp(a, absoluteposition);
-		ev3_command_motor_by_name(a, "run-to-abs-pos");
-
-		while (ev3_motor_state(a) & MOTOR_RUNNING);
-	}
-	fprintf(fp, "Motor opened\n");
-
-
-
-	ev3_delete_motors(motors);
-
-	fclose(fp);
-
-	return 0;
-}
-
-int testRobot()
+int test_motors()
 {
 	ev3_motor_ptr motors = ev3_load_motors();
 	ev3_motor_ptr motor = motors;
@@ -99,6 +28,69 @@ int testRobot()
 
 	fclose(fp);
 
+	return 0;
+}
+
+int test_sensors(){
+	int i;
+	//Loading all sensors
+	ev3_sensor_ptr sensors = ev3_load_sensors();
+	//I myself have a nxt sound sensor, which I detect here and set the
+	//correct driver. If you don't have an nxt analog sensor connected
+	//nothing should happen. If you have another than the sound sensor
+	//connected the values will be interpreted as sound values. Adjust
+	//this line in that case.
+	ev3_driver_sensor(
+		ev3_search_sensor_by_identifier( sensors, NXT_ANALOG, 0 ),
+		"lego-nxt-sound"
+	);
+	//Interating over the sensors and printing some interesting data
+	ev3_sensor_ptr sensor = sensors;
+	while (sensor)
+	{
+		printf("==== %s ====\n",sensor->driver_name);
+		printf("ident: %i\n",sensor->driver_identifier);
+		printf("sensor: %i\n",sensor->sensor_nr);
+		printf("port: %i\n",sensor->port);
+		printf("bin_fd: %i\n",sensor->bin_fd);
+		printf("data count: %i\n",sensor->data_count);
+		printf("data format: %i\n",sensor->bin_data_format);
+		printf("units: %s\n",sensor->units);
+		printf("decimals: %i\n",sensor->decimals);
+		printf("poll ms: %i\n",sensor->poll_ms);
+		printf("mode count: %i\n",sensor->mode_count);
+		for (i = 0; i < sensor->mode_count; i++)
+			printf("mode[%i]: %s\n",i,sensor->modes[i]);
+		printf("active mode: %i\n",sensor->mode);
+		//If there is more than one mode, let's choose the second one.
+		if (sensor->mode_count > 1)
+			ev3_mode_sensor(sensor,1);
+
+		//Let's open the sensors ;)
+		ev3_open_sensor(sensor);
+		sensor = sensor->next;
+	}
+	//Let's do this again, but this time we print the bin and val data
+	//values for every found sensor for the next 10 seconds
+	printf("Showing the sensor values for the next 10 seconds\n");
+	for (i = 0; i < 10; i++)
+	{
+		sensor = sensors;
+		while (sensor)
+		{
+			ev3_update_sensor_bin(sensor);
+			ev3_update_sensor_val(sensor);
+			printf("%s [%i]: \n",sensor->driver_name,sensor->port);
+			int j;
+			for (j = 0; j < sensor->data_count;j++)
+				printf("\tvalue %i: %i (raw) - %i (formated)\n",j,sensor->bin_data[0].s32,sensor->val_data[0].s32);
+			sensor = sensor->next;
+		}
+		sleep(1);
+	}
+	//Let's delete the list in the very end. It will also close the
+	//sensors
+	ev3_delete_sensors(sensors);
 	return 0;
 }
 */
