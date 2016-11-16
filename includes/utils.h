722057e39e2af770823c157a16c6d0713d11699d
alexis <alexis@shark>
2016-11-16 10:43:16 +0100

    Correct Axel errors.

--- includes/utils.h
+++ includes/utils.h
@@ -1,6 +1,7 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include "main.h"
 
 int init_bluetooth();
 
@@ -9,5 +10,7 @@ void close_inet(state *s);
 
 int send_message(state *s, int messageType, ...);
 char read_from_server(state *s, char *buffer);
+
+int clean_angle(int angle);
 
 #endif
--- src/utilspositions.c
+++ src/utilspositions.c
@@ -12,21 +12,23 @@
 #include "main.h"
 #include "config.h"
 #include "logger.h"
+#include "utils.h"
 
 void update_pos(state* s, int * pos, int angle) {
-	s->pos=pos;
-	s->angle = clean_angle(angle);
-	log_printf(s, "\n[UPDATE_POSITION] x=%d y=%d angle=%d\n", s->pos[0], s->pos[1], s->angle);
+	//s->pos=pos; CAREFUL s->pos is int[2]
+	s->angle = clean_angle(angle); //ADD DECLARATIONS TO UTILS.H
+	log_this(s, "\n[UPDATE_POSITION] x=%d y=%d angle=%d\n", s->pos[0], s->pos[1], s->angle);
 }
 void init_pos(state *s){
 	s->pos[0]=0;
 	s->pos[1]=1;
 	s->angle=0;
 }
+
+//DO you really want int for angles  ??
 int clean_angle(int angle) {
 	while (angle < 0) {
 		angle = angle + 360;
 	}
 	return angle % 360;
 }
-
