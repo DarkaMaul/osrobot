12590653b302931b6e991eaf7b5f6d071afbd113
Alexis <darkamaul@hotmail.fr>
2016-11-16 10:46:58 +0100

    Correct motor.h invalid declaration for go_straight

--- includes/motors.h
+++ includes/motors.h
@@ -14,6 +14,6 @@ void init_motors(state * s);
 int grab(state * s, int speed);
 int release(state * s, int speed);
 int go_to_position(state * s, int speed);
-int go_straight(state * s, int speed);
+int go_straight(state *s,int speed, int distance);
 
 #endif /* INCLUDES_MOTORS_H_ */
