228a4df76bd58972f8adbf73772a11b82684db73
astrubel <axel.strubel@telecom-bretagne.eu>
2016-11-21 15:09:52 +0100

    Modified filename with __file__ instead

--- src/motors.c
+++ src/motors.c
@@ -9,7 +9,6 @@
 #include "config.h"
 #include "utils.h"
 
-#define FILENAME "motors.c :"
 
 void init_motors(state *s){
     s->motors = ev3_load_motors();
@@ -24,13 +23,13 @@ void init_motors(state *s){
         ev3_open_motor(motor);
         ev3_reset_motor(motor);
         ev3_set_position(motor, 0);
-        log_this(s, "%s Motor %i on port %c opened, assigned and reseted\n", FILENAME,  motor->motor_nr, motor->port);
+        log_this(s, "%s Motor %i on port %c opened, assigned and reseted\n", __FILE__,  motor->motor_nr, motor->port);
         motor = motor->next;
     }
     ev3_stop_command_motor_by_name(s->grabmotor, "hold");
 
     init_pos(s);
-    log_this(s, "Position initialized to x:%i, y:%i\n", FILENAME,s->pos[0],s->pos[1]);
+    log_this(s, "Position initialized to x:%i, y:%i\n", __FILE__,s->pos[0],s->pos[1]);
 }
 
 //Grabbing functions
@@ -41,7 +40,7 @@ void init_motors(state *s){
 int grab(state *s, int speed)
 {
     if (ev3_get_position(s->grabmotor) == GRAB_POSITION){
-        log_this(s, "%s Grabbing failed already in grabbing position\n", FILENAME);
+        log_this(s, "%s Grabbing failed already in grabbing position\n", __FILE__);
         return -1;
     }
     ev3_set_speed_sp(s->grabmotor, speed);
@@ -60,7 +59,7 @@ int grab(state *s, int speed)
 int release(state *s, int speed)
 {
     if (ev3_get_position(s->grabmotor) == INIT_GRAB_POSITION){
-        log_this(s, "%s Releasing failed already in grabbing position\n", FILENAME);
+        log_this(s, "%s Releasing failed already in grabbing position\n", __FILE__);
         return -1;
     }
     ev3_set_speed_sp(s->grabmotor, speed);
@@ -81,19 +80,19 @@ int release(state *s, int speed)
 void set_wheels_speed(state *s, int speed){
     ev3_set_speed_sp(s->leftmotor, speed);
     ev3_set_speed_sp(s->rightmotor, speed);
-    log_this(s, "%s Wheels' speed set to %d \n", FILENAME, speed);
+    log_this(s, "%s Wheels' speed set to %d \n", __FILE__, speed);
 }
 
 void set_wheels_time(state *s, int time){
     ev3_set_time_sp(s->leftmotor, time);
     ev3_set_time_sp(s->rightmotor, time);
-    log_this(s, "%s Wheels' time set to %d \n", FILENAME, time);
+    log_this(s, "%s Wheels' time set to %d \n", __FILE__, time);
 }
 
 void set_wheels_pos(state *s, int pos){
     ev3_set_position_sp(s->leftmotor, pos);
     ev3_set_position_sp(s->rightmotor, pos);
-    log_this(s, "%s Wheels' position set to %d \n", FILENAME, pos);
+    log_this(s, "%s Wheels' position set to %d \n", __FILE__, pos);
 }
 
 /*
@@ -109,7 +108,7 @@ void command_wheels(state *s, int cmd){
  * Function to go for a given time at a given speed
  */
 int wheels_run_time(state *s, int speed, int time){
-    log_this(s, "%s Wheels running for %d s ...", FILENAME, time);
+    log_this(s, "%s Wheels running for %d s ...", __FILE__, time);
     set_wheels_speed(s, speed);
     set_wheels_time(s, time);
     command_wheels(s, RUN_TIMED);
@@ -122,7 +121,7 @@ int wheels_run_time(state *s, int speed, int time){
  * Function to go a given distance at a given speed
  */
 int wheels_run_pos(state *s, int speed, int pos){
-    log_this(s, "%s Wheels running to relative position %d...", FILENAME, pos);
+    log_this(s, "%s Wheels running to relative position %d...", __FILE__, pos);
     set_wheels_speed(s, speed);
     set_wheels_pos(s, pos);
     command_wheels(s, RUN_TO_REL_POS);
@@ -134,7 +133,7 @@ int wheels_run_pos(state *s, int speed, int pos){
  * Function to go straight
  */
 int go_straight(state *s, int speed, int distance){
-    log_this(s, "%s Going Straight for %d cm...", FILENAME, distance);
+    log_this(s, "%s Going Straight for %d cm...", __FILE__, distance);
     // deduce the angle from the given distance :
     int position = (distance*360)/(M_PI*WHEEL_DIAMETER);// wheels have diameter 5.6 cm
     return wheels_run_pos(s, speed, position);
@@ -166,5 +165,17 @@ int go_to_pos(state *s,int *desiredposition){
  * Function that will be used to turn the robot to the desired angle
  */
 int turn_to_desired_angle(state *s,int angle){
+	return 0;
+}
+
+/**
+ * Fonction to correct
+ */
+int correct_position(state *s){
+	s->gyro->val_data[0].s32;
+	int actualangle=s->gyro->val_data[0].s32;
+	if(!(s->angle-5<actualangle<s->angle+5)){
+		return turn(s,s->angle);
+	}
 	return 0;
 }
