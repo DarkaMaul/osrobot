4aa5f67fe7586e6b8e4e87ae0b7feb1a38088f3a
Hugo <hderambure@gmail.com>
2016-12-01 16:09:04 +0100

    Correct logger

--- src/logger.c
+++ src/logger.c
@@ -55,6 +55,6 @@ void log_this(state *s, const char *format, ...)
     va_list args;
     va_start(args, format);
     vfprintf(s->logfile_fd, format, args);
-    printf(format,args);
+    vprintf(format,args);
     va_end(args);
 }
