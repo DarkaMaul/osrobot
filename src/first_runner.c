eeae6c2ec05f775a7816aafe70ce5dbbe4713fd6
Alexis <darkamaul@hotmail.fr>
2016-12-06 15:13:09 +0100

    Refactor first runner

--- src/first_runner.c
+++ src/first_runner.c
@@ -8,9 +8,11 @@
 int first_runner_small_stadium(state *s)
 {
     //Init the Game
-	position init;
-    init.x = S_FR_S_0_X;
-    init.y = S_FR_S_0_Y +  WHEELS_TO_END;
+	position init = {
+        .x = S_FR_S_0_X,
+        .y = S_FR_S_0_Y +  WHEELS_TO_END		
+    };
+
     update_pos(s, init);
 
     //TODO Maybe to put it by default at 90
@@ -20,9 +22,10 @@ int first_runner_small_stadium(state *s)
     //catch_ball(s);
 
     //Go to center
-	position releaseballposition;
-	releaseballposition.x = S_BA_0_X + (S_BA_3_X -  S_BA_0_X) / 2;
-	releaseballposition.y = S_BA_3_Y + (S_BA_2_Y - S_BA_3_Y) /2 ;
+	position releaseballposition = {
+        .x = S_BA_0_X + (S_BA_3_X -  S_BA_0_X) / 2,
+        .y = S_BA_3_Y + (S_BA_2_Y - S_BA_3_Y) /2 
+    };
 
 	go_to_pos(s, releaseballposition);
 
@@ -32,14 +35,15 @@ int first_runner_small_stadium(state *s)
 	go_straight(s, MAX_WHEEL_SPEED, -30);
 
 	//Go to ending position
-	position endingposition;
-	endingposition.x=S_FR_E_0_X;
-	endingposition.y= (S_FR_E_0_Y + S_FR_E_1_Y) / 2;
+	position endingposition = {
+        .x = S_FR_E_0_X,
+        .y = (S_FR_E_0_Y + S_FR_E_1_Y) / 2
+    };
 
 	go_to_pos(s, endingposition);
 
 	//Send ok signal bluetooth for other team
-
+    send_message(s, MSG_NEXT, s->ally);
     return 0;
 }
 
