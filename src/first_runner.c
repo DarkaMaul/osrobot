0a119ea8770af4d36d4a98482d400fccd2443b6d
hderambure <hderambure@gmail.com>
2016-12-12 16:05:08 +0100

    Add obstacles' coordinates and clean positions in first runner

--- src/first_runner.c
+++ src/first_runner.c
@@ -26,11 +26,11 @@ mainpos init_main_positions(){
 	position s_fr_init={.x = S_FR_S_0_X,.y = S_FR_S_0_Y +  WHEELS_TO_END};
 	position s_fr_releaseballposition = {.x = S_BA_0_X - 5,.y = S_BA_0_Y - WHEELS_TO_END + 5};
 	position s_fr_endingposition = {.x = S_FR_E_0_X,.y = ( S_FR_E_0_Y + WHEELS_TO_END) + 15};
-	position l_fr_init = {.x = L_FR_S_0_X + 10, .y = L_FR_S_0_Y + WHEELS_TO_END};
-	position l_fr_dodgefirst = {.x = L_FR_S_0_X + 70, .y = L_FR_S_0_Y + WHEELS_TO_END + 155};
-	position l_fr_center = {.x = L_BA_0_X + WHEELS_TO_END/2, .y = L_BA_0_Y - WHEELS_TO_END+20};
-	position l_fr_dodgesecond = {.x = L_FR_E_0_X , .y = L_FR_E_0_Y + WHEELS_TO_END - 130};
-	position l_fr_ending = {.x = L_FR_E_0_X, .y = L_FR_E_0_Y + WHEELS_TO_END + 20};
+	position l_fr_init = {.x = L_FR_S_0_X + 10, .y = L_FR_S_0_Y + WHEELS_TO_END}; // {.x = L_FR_S_0_X + WHEELS_TO_END, .y = L_FR_S_0_Y + WHEELS_TO_END}
+	position l_fr_dodgefirst = {.x = L_FR_S_0_X + 70, .y = L_FR_S_0_Y + WHEELS_TO_END + 155}; //{.x = L_O1_2_X + L_FR_S_0_X, .y = L_O1_2_Y + L_FR_S_0_Y}
+	position l_fr_center = {.x = L_BA_0_X + WHEELS_TO_END/2, .y = L_BA_0_Y - WHEELS_TO_END+20}; // {.x = L_BA_0_X + L_FR_0_X/2, .y = L_BA_0_Y + L_FR_S_Y}
+	position l_fr_dodgesecond = {.x = L_FR_E_0_X , .y = L_FR_E_0_Y + WHEELS_TO_END - 130}; // {.x = L_FR_E_0_X , .y = L_02_0_Y}
+	position l_fr_ending = {.x = L_FR_E_0_X, .y = L_FR_E_0_Y + WHEELS_TO_END + 20}; //{.x = L_FR_E_1_X, .y = L_FR_E_1_Y};
 
 	positions.s_fr_init=s_fr_init;
 	positions.s_fr_releaseball=s_fr_releaseballposition;
