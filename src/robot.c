1e810591b40a697f4661d68f73736b3161bde62d
astrubel <axel.strubel@telecom-bretagne.eu>
2017-01-05 16:37:07 +0100

    modified look for ball to look only for the ball

--- src/robot.c
+++ src/robot.c
@@ -44,16 +44,64 @@ int catch_ball(state* s)
 
     return returnValue;
 }
+
 /**
- * Function to look for the ball while moving (or not)
+ * Function to look for the ball when close to the ball area
  * @param s State of LeE
- * @return 0 if ball is found 1 otherwise
+ * @return angle if ball is found SONAR_ERROR_ANGLE otherwise
  */
 int look_for_ball(state *s){
 	int distanceToBallorObstacle = distance_from_obstacle(s);
 	int turn_sweep= MAX_SWEEP_ANGLE;
     log_this(s, "[%s] Look for ball started\n", __FILE__);
     log_this(s, "[%s] Distance to ball or obstacle %d\n", __FILE__, distanceToBallorObstacle);
+	sweep_absolute(s, 100, MAX_SWEEP_ANGLE);
+	int sweep_angle=-SWEEP_ANGLE;
+	while((distanceToBallorObstacle == -1 || distanceToBallorObstacle > 50) && abs(turn_sweep) < MAX_SWEEP_ANGLE)
+	{
+		turn_sweep+=sweep_angle;
+		//Positive for clockwise turn
+		sweep_absolute(s, 100, turn_sweep);
+		usleep(200000);
+		distanceToBallorObstacle = distance_from_obstacle(s);
+	    log_this(s, "[%s] Distance to ball %d\n", __FILE__, distanceToBallorObstacle);
+	}
+	if(abs(turn_sweep) >= MAX_SWEEP_ANGLE) return SONAR_ERROR_ANGLE;
+
+	//angle one detected is the first angle where the ball appears
+	int angle_one_detected=turn_sweep;
+    log_this(s, "[%s] First angle where ball is detected %d\n", __FILE__, angle_one_detected);
+
+    int extra_max_sweep_angle=MAX_SWEEP_ANGLE+20;
+	while(distanceToBallorObstacle < 50 && abs(turn_sweep) < extra_max_sweep_angle)
+	{
+		turn_sweep+=sweep_angle;
+		//Positive for clockwise turn added 20 degrees if ball is in the limit of the sweep angle
+		sweep_absolute(s, 100, turn_sweep);
+		usleep(200000);
+		distanceToBallorObstacle = distance_from_obstacle(s);
+		log_this(s, "[%s] Distance to ball or obstacle %d\n", __FILE__, distanceToBallorObstacle);
+	}
+	int angle_two_lost=turn_sweep;
+    log_this(s, "[%s] Second angle where ball is not detected anymore %d\n", __FILE__, angle_two_lost);
+    int bissect_angle=(angle_one_detected-angle_two_lost)/2;
+    log_this(s, "[%s] Calculated bissectrice to turn to detect ball %d\n", __FILE__, angle_two_lost);
+	//replace the motors to the original position
+    sweep_absolute(s, 100, MAX_SWEEP_ANGLE);
+    return bissect_angle;
+}
+
+/**
+ * Function to look for an obstacle while moving (or not)
+ * @param s State of LeE
+ * @return 0 if obstacle is found 1 otherwise
+ */
+/*
+int look_for_obstacle(state *s){
+	int distanceToBallorObstacle = distance_from_obstacle(s);
+	int turn_sweep= MAX_SWEEP_ANGLE;
+    log_this(s, "[%s] Look for obstacle started\n", __FILE__);
+    log_this(s, "[%s] Distance to obstacle or obstacle %d\n", __FILE__, distanceToBallorObstacle);
 	sweep_absolute(s, 100, MAX_SWEEP_ANGLE);
 	int sweep_angle=-SWEEP_ANGLE;
 	while(distanceToBallorObstacle == -1 || distanceToBallorObstacle > 50)
@@ -70,25 +118,10 @@ int look_for_ball(state *s){
 		distanceToBallorObstacle = distance_from_obstacle(s);
 	    log_this(s, "[%s] Distance to ball or obstacle %d\n", __FILE__, distanceToBallorObstacle);
 	}
-	//angle one detected is the first angle where the ball appears
+	//angle one detected is the first angle where the obstacle appears
 	int angle_one_detected=turn_sweep;
-    log_this(s, "[%s] First angle where ball is detected %d\n", __FILE__, angle_one_detected);
-    int extra_max_sweep_angle=MAX_SWEEP_ANGLE+20;
-	while(distanceToBallorObstacle < 50 && abs(turn_sweep) < extra_max_sweep_angle)
-	{
-		turn_sweep+=sweep_angle;
-		//Positive for clockwise turn added 20 degrees if ball is in the limit of the sweep angle
-		sweep_absolute(s, 100, turn_sweep);
-		usleep(200000);
-		distanceToBallorObstacle = distance_from_obstacle(s);
-		log_this(s, "[%s] Distance to ball or obstacle %d\n", __FILE__, distanceToBallorObstacle);
-	}
-	int angle_two_lost=turn_sweep;
-    log_this(s, "[%s] Second angle where ball is not detected anymore %d\n", __FILE__, angle_two_lost);
-    int bissect_angle=(angle_one_detected-angle_two_lost)/2;
-    log_this(s, "[%s] Calculated bissectrice to turn to detect ball %d\n", __FILE__, angle_two_lost);
-
-    return bissect_angle;
+    return 0;
 }
+*/
 
 
