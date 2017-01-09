b51ecccd779e0d71de5167ded0ea58849de1b629
astrubel <axel.strubel@telecom-bretagne.eu>
2017-01-09 16:09:15 +0100

    Modification of the bissector calculated if its in an extreme position

--- src/robot.c
+++ src/robot.c
@@ -6,8 +6,8 @@
 #include "config.h"
 #include "motors.h"
 #include "logger.h"
-/**
 
+/**
  * Catch the ball if it's in front of the robot
  * @param  s State structure
  * @return   BALL_FOUND || BALL_NOT_FOUND
@@ -71,9 +71,16 @@ int look_for_ball_in_close_perimeter(state *s){
 	//angle one detected is the first angle where the ball appears
 	int angle_one_detected=turn_sweep;
     log_this(s, "[%s] First angle where ball is detected %d\n", __FILE__, angle_one_detected);
+    //Angle is too close to the limit so return the first angle found
+    if(abs(angle_one_detected)+ERROR_DISTANCE_MARGIN >= MAX_SWEEP_ANGLE){
+    	int bissect_angle= angle_one_detected;
+    	log_this(s, "[%s] Not calculating second angle ball is too close to the limit of the sweeper capability. Ball at angle=%d\n", __FILE__, bissect_angle);
+    	sweep_absolute(s, 100, 0);
+    	return bissect_angle;
+    }
 
     int extra_max_sweep_angle=MAX_SWEEP_ANGLE+10;
-	while(distanceToBallorObstacle < GAP_MIN_BETWEEN_ROBOT_BALL && abs(turn_sweep) < extra_max_sweep_angle && distanceToBallorObstacle!=-1)
+	while(distanceToBallorObstacle <= GAP_MIN_BETWEEN_ROBOT_BALL && abs(turn_sweep) < extra_max_sweep_angle && distanceToBallorObstacle!=-1)
 	{
 		turn_sweep+=sweep_angle;
 		//Positive for clockwise turn added 20 degrees if ball is in the limit of the sweep angle
@@ -84,9 +91,11 @@ int look_for_ball_in_close_perimeter(state *s){
 	}
 	int angle_two_lost=turn_sweep;
     log_this(s, "[%s] Second angle where ball is not detected anymore %d\n", __FILE__, angle_two_lost);
-    int bissect_angle= -(angle_one_detected+angle_two_lost-sweep_angle)/2;
+
+    int bissect_angle= -(angle_one_detected+angle_two_lost-sweep_angle)/2-ERROR_MARGIN;
     log_this(s, "[%s] Calculated bisector to turn to detect ball %d\n", __FILE__, bissect_angle);
-	//replace the motors to the original position
+
+    //replace the motors to the original position
     sweep_absolute(s, 100, 0);
     return bissect_angle;
 }
@@ -103,7 +112,8 @@ int look_for_ball(state *s){
 	//TODO parameters can be adjusted to search more
 	int nb_of_steps=1;
 	int size_of_steps=20;
-	for (int i=0;i<nb_of_steps;i++){
+	int i;
+	for (i=0;i<nb_of_steps;i++){
 		if (angle_to_ball == SONAR_ERROR_ANGLE)
 		{
 			go_straight(s, MAX_WHEEL_SPEED, size_of_steps);
@@ -122,10 +132,9 @@ int look_for_ball(state *s){
 	    log_this(s, "[%s] Look for ball failed there is nothing in front of the robot\n", __FILE__);
 		return -1;
 	}
+    log_this(s, "[%s] Look for ball succeeded there is something close to the robot\n", __FILE__);
 	return 0;
 }
-
-
 
 
 /**
