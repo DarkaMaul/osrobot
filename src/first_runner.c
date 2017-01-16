26a593d5c4334ef5c1e1094ddaf893f9f22889f6
hderambure <hderambure@gmail.com>
2017-01-16 16:44:51 +0100

    Change center to ball area in finishers + clean some logs

--- src/first_runner.c
+++ src/first_runner.c
@@ -73,25 +73,25 @@ int beginner_small_stadium(state *s, mainpos *p)
     update_pos(s, p->s_fr_init);
 
     //By default we are in realeasing position so just close the clamps when starting
-    log_this(s,"\n\n[%s: beginner_small_stadium] Grabbing the ball\n\n", __FILE__);
+    log_this(s,"\n[%s:beginner_small_stadium] Grabbing the ball\n", __FILE__);
     grab(s, MAX_GRABBING_SPEED);
 
     //Go to center
-    log_this(s,"\n\n[%s: beginner_small_stadium] Going to ball area\n\n", __FILE__);
+    log_this(s,"\n[%s:beginner_small_stadium] Going to ball area\n", __FILE__);
     go_to_pos(s, p->s_fr_ballarea);
     sleep(2);
 
     //release ball
-    log_this(s,"\n\n[%s: beginner_small_stadium] Releasing ball\n\n", __FILE__);
+    log_this(s,"\n[%s:beginner_small_stadium] Releasing ball\n", __FILE__);
     release(s, RELEASING_SPEED);
     send_message(s, MSG_BALL, s->ally);
 
 	//Go back a little
-    log_this(s, "\n\n[%s: beginner_small_stadium] Going back a little\n\n", __FILE__);
+    log_this(s, "\n[%s:beginner_small_stadium] Going back a little\n", __FILE__);
 	go_straight(s, MAX_WHEEL_SPEED, -20);
 
 	//Go to ending position
-    log_this(s, "\n\n[%s: beginner_small_stadium] Going to the end\n\n", __FILE__);
+    log_this(s, "\n[%s:beginner_small_stadium] Going to the end\n", __FILE__);
     go_to_pos(s, p->s_fr_ending);
 
 	//Send ok signal bluetooth for other team
@@ -108,34 +108,34 @@ int beginner_large_stadium(state *s, mainpos *p)
     update_pos(s, p->l_fr_init);
 
     //By default we are in realeasing position so just close the clamps when starting
-    log_this(s, "\n[%s:beginner_large_stadium] Grabbing the ball \n\n",__FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Grabbing the ball \n",__FILE__);
     grab(s, MAX_GRABBING_SPEED);
 
     //Dodge first obstacle
-    log_this(s, "\n[%s:beginner_large_stadium] Dodging first obstacle \n\n",__FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Dodging first obstacle \n",__FILE__);
 	go_to_pos(s, p->l_fr_dodgefirst);
 
     //Go to center and a 180
-    log_this(s, "\n[%s:beginner_large_stadium] Going to the center\n\n",__FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Going to the center\n",__FILE__);
     go_to_pos(s, p->l_fr_ballarea);
     turn(s, TURNING_SPEED, 180);
 
     //Release ball
     usleep(100000);
-    log_this(s, "\n[%s:beginner_large_stadium] Releasing the ball\n\n",__FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Releasing the ball\n",__FILE__);
     release(s, MAX_GRABBING_SPEED);
     send_message(s, MSG_BALL, s->ally);
 
    	//Go back a little
-    log_this(s, "\n[%s: beginner_large_stadium] Going back a little\n\n", __FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Going back a little\n", __FILE__);
 	go_straight(s, MAX_WHEEL_SPEED, -20);
 
     //Dodge second stadium
-    log_this(s, "\n[%s:beginner_large_stadium] Dodging second obstacle\n\n" ,__FILE__);
+    log_this(s, "\n[%s:beginner_large_stadium] Dodging second obstacle\n" ,__FILE__);
 	go_to_pos(s, p->l_fr_dodgesecond);
 
     //Go to ending position
-    log_this(s, "\n\n[%s: beginner_large_stadium] Going to the end\n\n", __FILE__);
+    log_this(s, "\n[%s: beginner_large_stadium] Going to the end\n", __FILE__);
     go_to_pos(s, p->l_fr_ending);
 
 
@@ -153,21 +153,22 @@ int finisher_small_stadium(state *s, mainpos *p)
     update_angle(s,90);
     s->gyro_reference -= 180; //To set the starting gyro value to 90° (clockwise)
 
-    //TODO replace center by ball position sent by the beginner
+
     //Go to center
-    log_this(s,"\n\n[%s: finisher_small_stadium] Going to ball area\n\n", __FILE__);
-    go_to_pos(s, p->s_sr_ballarea);
+    log_this(s,"\n[%s:finisher_small_stadium] Going to ball area\n", __FILE__);
+    //go_to_pos(s, p->s_sr_ballarea);
+    go_to_pos(compute_arrival_point(s));
 
     //Look for the ball
-    log_this(s,"\n\n[%s: finisher_small_stadium] Looking for the ball\n\n", __FILE__);
+    log_this(s,"\n[%s:finisher_small_stadium] Looking for the ball\n", __FILE__);
     look_for_ball(s);
 
     //Catch the ball
-    log_this(s,"\n\n[%s: finisher_small_stadium] Catching the ball\n\n", __FILE__);
+    log_this(s,"\n[%s:finisher_small_stadium] Catching the ball\n", __FILE__);
     catch_ball(s);
 
   	//Go to ending position
-    log_this(s, "\n\n[%s: finisher_small_stadium] Going to the end\n\n", __FILE__);
+    log_this(s, "\n[%s:finisher_small_stadium] Going to the end\n", __FILE__);
     go_to_pos(s, p->s_sr_ending);
 
 	//Send ok signal bluetooth for other team
@@ -184,26 +185,26 @@ int finisher_large_stadium(state *s, mainpos *p)
     update_pos(s, p->l_fr_init);
 
     //Dodge first obstacle
-    log_this(s, "\n[%s:finisher_large_stadium] Dodging first obstacle \n\n",__FILE__);
+    log_this(s, "\n[%s:finisher_large_stadium] Dodging first obstacle \n",__FILE__);
 	go_to_pos(s, p->l_fr_dodgefirst);
 
     //Go to center and a 180
-    log_this(s, "\n[%s:finisher_large_stadium] Going to the center\n\n",__FILE__);
-    go_to_pos(s, p->l_fr_ballarea);
-    turn(s, TURNING_SPEED, 180);
+    log_this(s, "\n[%s:finisher_large_stadium] Going to the center\n",__FILE__);
+    //go_to_pos(s, p->l_fr_ballarea);
+    go_to_pos(compute_arrival_point(s));
 
     //Look for the ball
-    log_this(s,"\n\n[%s: finisher_large_stadium] Looking for the ball\n\n", __FILE__);
+    log_this(s,"\n[%s:finisher_large_stadium] Looking for the ball\n", __FILE__);
     look_for_ball(s);
 
     //Catch the ball
-    log_this(s,"\n\n[%s: finisher_large_stadium] Catching the ball\n\n", __FILE__);
+    log_this(s,"\n[%s:finisher_large_stadium] Catching the ball\n", __FILE__);
     catch_ball(s);
 
 
 
     //Go to ending position
-    log_this(s, "\n\n[%s: finisher_large_stadium] Going to the end\n\n", __FILE__);
+    log_this(s, "\n[%s: finisher_large_stadium] Going to the end\n", __FILE__);
     go_to_pos(s, p->l_fr_ending);
 
 	//Send ok signal bluetooth for other team
